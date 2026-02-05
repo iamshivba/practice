ac1599a836b93fbdcb725346b4e12c25b5c4b9a2
iamshivba <iamsnk2209@gmail.com>
2026-02-05 14:52:54 +0530

    Update main.tf
    add subnet and data

--- main.tf
+++ main.tf
@@ -10,3 +10,14 @@ terraform {
 provider "aws" {
   region = "ap-south-1"
 }
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
