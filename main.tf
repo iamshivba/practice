c599ec83f60ffd73dd1d521c4c30a2addb47d040
iamshivba <iamsnk2209@gmail.com>
2026-02-06 13:46:24 +0530

    Update main.tf
    add key for ssh

--- main.tf
+++ main.tf
@@ -122,6 +122,7 @@ resource "aws_launch_template" "app_lt" {
   name_prefix   = "app-lt-"
   image_id      = data.aws_ami.amazon_linux.id
   instance_type = "t3.micro"
+  key_name = "shiv_key"
 
   vpc_security_group_ids = [
     aws_security_group.ec2_sg.id
