1e813735424a45b5a2f91f36ddde5186e1c8841b
iamshivba <iamsnk2209@gmail.com>
2026-02-06 11:18:34 +0530

    Update main.tf
    Add target group

--- main.tf
+++ main.tf
@@ -63,3 +63,21 @@ resource "aws_security_group" "ec2_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+
+resource "aws_lb_target_group" "app_tg" {
+  name     = "app-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id  = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
