cd2e1361ef0aeddd7da1eefdb604434899406b68
iamshivba <iamsnk2209@gmail.com>
2026-02-06 11:26:20 +0530

    Update main.tf
    Add load balalncer and listner

--- main.tf
+++ main.tf
@@ -81,3 +81,24 @@ resource "aws_lb_target_group" "app_tg" {
     unhealthy_threshold = 2
   }
 }
+
+resource "aws_lb" "app_alb" {
+  name               = "app-alb"
+  load_balancer_type = "application"
+  internal           = false
+
+  security_groups = [aws_security_group.alb_sg.id]
+  subnets         = data.aws_subnets.default.ids
+}
+
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
+
