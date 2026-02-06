36a24805d6669cc4e0a44ccf49d39fc568961363
iamshivba <iamsnk2209@gmail.com>
2026-02-06 13:09:45 +0530

    Update main.tf
    Add alarm using scale out scale in

--- main.tf
+++ main.tf
@@ -153,3 +153,38 @@ resource "aws_autoscaling_group" "app_asg" {
   health_check_grace_period = 120
 }
 
+resource "aws_cloudwatch_metric_alarm" "cpu_high" {
+  alarm_name          = "asg-cpu-high"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 70
+
+  alarm_description = "Scale out if CPU > 70% for 2 minutes"
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.app_asg.name
+  }
+}
+
+
+resource "aws_cloudwatch_metric_alarm" "cpu_low" {
+  alarm_name          = "asg-cpu-low"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = 5
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 30
+
+  alarm_description = "Scale in if CPU < 30% for 5 minutes"
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.app_asg.name
+  }
+}
+
